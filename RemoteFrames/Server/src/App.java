d16e1f1ae51aecb01e6f2f7a9215eff0aa41b8b6
Daniel Kletinich <kletinich@gmail.com>
2025-01-07 11:42:06 +0200

    removed outdated projects

--- RemoteFrames/Server/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        server.Start();
-        server.work();
-    }
-}
--- RemoteFrames/remoteDesktop/src/App.java
+++
@@ -1,9 +0,0 @@
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Client client = new Client();
-        client.connectToServer();
-        client.work();
-    }
-}
-
